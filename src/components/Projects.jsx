cb040b274680578a29f10ecab2c1347f0b97b99f
edsng <qhostgg@gmail.com>
2024-10-13 14:33:40 -0700

    added cards to projects section and clickable links

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../css/Projects.css';
 import { motion } from 'framer-motion';
+import { FaArrowRight } from 'react-icons/fa'; // Added Font Awesome up arrow import
 
 // Consolidate variants for better readability and sequential animations
 const containerVariants = {
@@ -35,6 +36,35 @@ const Projects = () => {
         <motion.h1 variants={itemVariants}>korean american<br /></motion.h1>
         <motion.h2 variants={itemVariants}>software engineer based in los angeles</motion.h2>
         <motion.p variants={itemVariants}>Computer Science @ University of California, Riverside</motion.p>
+      </motion.div>
+      <motion.div 
+        className="card-container" 
+        variants={containerVariants}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+      >
+        <motion.div className="project-card" variants={itemVariants}>
+          <a href="https://edsng.github.io/fynance" target="_blank" rel="noopener noreferrer">
+            <h3>Fynance</h3>
+            <p>AI-powered financial planning</p>
+            <FaArrowRight className="arrow-icon" /> {/* Added FA up arrow */}
+          </a>
+        </motion.div>
+        <motion.div className="project-card" variants={itemVariants}>
+          <a href="https://github.com/edsng/Bitcoin-Price-Forecasting-using-Machine-Learning-Models" target="_blank" rel="noopener noreferrer">
+            <h3>Quant</h3>
+            <p>Various quantitative finance projects</p>
+            <FaArrowRight className="arrow-icon" /> {/* Added FA up arrow */}
+          </a>
+        </motion.div>
+        <motion.div className="project-card" variants={itemVariants}>
+          <a href="https://github.com/edsng/CarPhoto" target="_blank" rel="noopener noreferrer">
+            <h3>CarPhoto</h3>
+            <p>Client matching web app for car enthusiasts</p>
+            <FaArrowRight className="arrow-icon" /> {/* Added FA up arrow */}
+          </a>
+        </motion.div>
       </motion.div>
     </div>
   );
