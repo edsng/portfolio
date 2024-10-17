de48b99cddcfabec744ddb5d61921e9a3f4460b0
edsng <qhostgg@gmail.com>
2024-10-17 09:59:22 -0700

    Added resume and experience section

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,7 +1,9 @@
 import React, { useRef, useEffect } from 'react';
 import { motion } from 'framer-motion';
-import { FaJava, FaPython, FaRegDotCircle, FaCircle} from 'react-icons/fa';
+import { FaArrowRight, FaRegDotCircle, FaCircle } from 'react-icons/fa';
 import '../css/Info.css';
+import rapidnetworks from '../assets/rapidnetworks.svg';
+import nexg from '../assets/nexg.svg';
 
 function useScrollSnap(ref) {
   useEffect(() => {
@@ -108,13 +110,44 @@ function Info() {
                 </motion.div>
             </motion.div>
             <motion.div 
-                className="info-container snap-section"
+                className="experience-container snap-section"
                 variants={fadeInVariant}
                 initial="hidden"
                 whileInView="visible"
                 viewport={{ once: true, amount: 0.3 }}
             >
-                <motion.h2 variants={infoItem}>Experience</motion.h2>
+                <motion.h1 variants={infoItem}>Experience</motion.h1>
+                <motion.div className="experience-card" variants={fadeInVariant}>
+                    <motion.div className="experience-logo" variants={fadeInVariant}>
+                        <motion.img src={rapidnetworks} style={{paddingRight: '10px'}} alt="Experience Logo" />
+                    </motion.div>
+                    <motion.div className="experience-text" variants={fadeInVariant}>
+                        <motion.h2 variants={infoItem}>Front-End Engineer</motion.h2>
+                        <motion.h3 variants={infoItem}>Rapid Networks</motion.h3>
+                        <motion.p variants={infoItem}>2024 - Los Angeles, CA</motion.p>
+                    </motion.div>
+                </motion.div>
+                <motion.div className="experience-card" variants={fadeInVariant}>
+                    <motion.div className="experience-text nexg" style={{textAlign: 'end'}} variants={fadeInVariant}>
+                        <motion.h2 variants={infoItem}>Technical Support Engineer</motion.h2>
+                        <motion.h3 variants={infoItem}>Network Expert Group</motion.h3>
+                        <motion.p variants={infoItem}>2020 - Los Angeles, CA</motion.p>
+                    </motion.div>
+                    <motion.div className="experience-logo" variants={fadeInVariant}>
+                        <motion.img src={nexg} alt="Experience Logo" />
+                    </motion.div>
+                </motion.div>
+                <motion.div className="experience-card" variants={fadeInVariant}>
+                    <motion.div className="experience-logo" variants={fadeInVariant}>
+                        <motion.img src={rapidnetworks} style={{paddingRight: '10px'}} alt="Experience Logo" />
+                    </motion.div>
+                    <motion.div className="experience-text" variants={fadeInVariant}>
+                        <motion.h2 variants={infoItem}>Logistics Staff</motion.h2>
+                        <motion.h3 variants={infoItem}>Rapid Networks</motion.h3>
+                        <motion.p variants={infoItem}>2020 - Los Angeles, CA</motion.p>
+                    </motion.div>
+                </motion.div>
+                <motion.a href={"https://docs.google.com/document/d/1IXSPeeWzdQOGPxRNA1LVZF2PR7a-5xvlymQfJBaZaFc/edit?usp=sharing"} className="resume-button" variants={infoItem} target="_blank" rel="noopener noreferrer">resume <FaArrowRight style={{transform: 'rotate(-45deg)', paddingBottom: '2px'}}/></motion.a>
             </motion.div>
         </div>
     );
